200c3bdbccd8c506c4a57ef481ba74626673f5a2
Blackcat-404 <tborisenko344@gmail.com>
2025-07-02 21:45:50 +0300

    Linking matrices with zeros and ones to fields in the grid, animation, adding an icon, starting/stopping the program by keystroke.

--- include/logic.h
+++ include/logic.h
@@ -1,20 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-static int** CreateField(const int width, const int height) {
-    int** field = (int**)malloc(height * sizeof(int*));
-    for (int i = 0; i < height; i++) {
-        field[i] = (int*)malloc(width * sizeof(int));
-        for (int j = 0; j < width; j++) {
+static bool** CreateField(const unsigned short width, const unsigned short height) {
+    bool** field = (bool**)malloc(height * sizeof(bool*));
+    for (unsigned short i = 0; i < height; i++) {
+        field[i] = (bool*)malloc(width * sizeof(bool));
+        for (unsigned short j = 0; j < width; j++) {
             field[i][j] = 0;
         }
     }
     return field;
 }
 
-static void NextStep(int** current, int** next, int width, int height) {
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            int alive_neighbors = 0;
+static void NextStep(bool** current, bool** next, unsigned short width, unsigned short height) {
+    for (unsigned short y = 0; y < height; y++) {
+        for (unsigned short x = 0; x < width; x++) {
+            unsigned short alive_neighbors = 0;
             for (int dy = -1; dy <= 1; dy++) {
                 for (int dx = -1; dx <= 1; dx++) {
                     if (dx == 0 && dy == 0) continue;
@@ -29,9 +33,17 @@ static void NextStep(int** current, int** next, int width, int height) {
             }
             if (current[y][x] == 1) {
                 next[y][x] = (alive_neighbors == 2 || alive_neighbors == 3) ? 1 : 0;
-            } else {
+            }
+            else {
                 next[y][x] = (alive_neighbors == 3) ? 1 : 0;
             }
         }
     }
+}
+
+void FreeField(bool** field, unsigned short size) {
+    for (unsigned short i = 0; i < size; i++) {
+        free(field[i]);
+    }
+    free(field);
 }
--- main.c
+++ main.c
@@ -1,50 +1,41 @@
 #include "include/GLFW/glfw3.h"
 #include "include/logic.h"
-#include <stdio.h>
 
 #define WIDTH 700
 #define HEIGHT 700
+
+static bool** CreateField(const unsigned short width, const unsigned short height);
+static void NextStep(bool** current, bool** next, unsigned short width, unsigned short height);
+void FreeField(bool** field, unsigned short size);
+
 
 void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    int side = (width < height) ? width-50 : height-50;
-    int offsetX = (width - side) / 2;
-    int offsetY = (height - side) / 2;
+    unsigned short side = (width < height) ? width-50 : height-50;
+    unsigned short offsetX = (width - side) / 2;
+    unsigned short offsetY = (height - side) / 2;
     glViewport(offsetX, offsetY, side, side);
 }
 
-void drawGrid(int gridSize) {    
-    float margin = 0.05f;
-    float gridWidth = WIDTH * (1 - 2*margin);
-    float gridHeight = HEIGHT * (1 - 2*margin);
-    
-    float startX = WIDTH * margin;
-    float startY = HEIGHT * margin;
-    
-    glColor3f(0.7f, 0.7f, 0.7f);
-    glLineWidth(1.0f);
-    
-    float cellSize = gridWidth / gridSize;
-    
-    glBegin(GL_LINES);
-    
-    for (int i = 0; i <= gridSize; i++) {
-        float x = startX + i * cellSize;
-        glVertex2f(x, startY);
-        glVertex2f(x, startY + gridHeight);
+static void setWindowIcon(GLFWwindow* window, const char* iconPath) {
+    GLFWimage image;
+    image.pixels = stbi_load(iconPath, &image.width, &image.height, 0, 4);
+    if (image.pixels) {
+        glfwSetWindowIcon(window, 1, &image);
+        stbi_image_free(image.pixels);
     }
-    
-    for (int j = 0; j <= gridSize; j++) {
-        float y = startY + j * cellSize;
-        glVertex2f(startX, y);
-        glVertex2f(startX + gridWidth, y);
-    }
-    
-    glEnd();
 }
+
+const unsigned short gridSize = 30; // 30x30
+const float margin = 0.01f;
+const float startX = WIDTH * margin;
+const float startY = HEIGHT * margin;
+const float gridWidth = WIDTH * (1 - 2*margin);
+const float gridHeight = HEIGHT * (1 - 2*margin);
+const float cellSize = gridWidth / gridSize;
 
 void drawRectangle(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
     glBegin(GL_QUADS);
@@ -58,7 +49,63 @@ void drawRectangle(float x1, float y1, float x2, float y2, float x3, float y3, f
     glEnd();
 }
 
-const int gridSize = 30; // 30x30
+void drawGrid(unsigned short gridSize) {    
+
+    glColor3f(0.7f, 0.7f, 0.7f);
+    glLineWidth(1.0f);
+    
+    float cellSize = gridWidth / gridSize;
+    
+    glBegin(GL_LINES);
+    
+    for (unsigned short i = 0; i <= gridSize; i++) {
+        float x = startX + i * cellSize;
+        glVertex2f(x, startY);
+        glVertex2f(x, startY + gridHeight);
+    }
+    
+    for (unsigned short j = 0; j <= gridSize; j++) {
+        float y = startY + j * cellSize;
+        glVertex2f(startX, y);
+        glVertex2f(startX + gridWidth, y);
+    }
+
+    glEnd();
+}
+
+static void fillField(bool** field, unsigned short size) {
+    for(unsigned short i = 0; i < size; i++) {
+        for (unsigned short j = 0; j < size; j++) {
+            if (field[i][j] == 1) {
+                unsigned short x = j, y = size - 1 - i;
+                drawRectangle(startX + x * cellSize, startY + y * cellSize, 
+                              startX + (x + 1) * cellSize, startY + y * cellSize, 
+                              startX + (x + 1) * cellSize, startY + (y + 1) * cellSize, 
+                              startX + x * cellSize, startY + (y + 1) * cellSize);
+            }
+        }
+    }
+}
+
+static void swapFields(bool ***current, bool ***next, unsigned short size) {
+    bool **temp = *current;
+    *current = *next;
+    *next = temp;
+}
+
+double lastStepTime = 0.0;
+float stepDelay = 0.05;
+bool shouldWait = true;
+
+static void key_space(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+       shouldWait = !shouldWait;
+    }
+    else false;
+}
+
+
 
 int main(void){
 
@@ -70,6 +117,8 @@ int main(void){
         return -1;
 
     window = glfwCreateWindow(WIDTH,HEIGHT, "Game of Life", NULL, NULL);
+
+    setWindowIcon(window, "icon.png");
 
     if (!window)
     {
@@ -85,11 +134,28 @@ int main(void){
 
     glfwSetWindowSizeLimits(window, 550, 450, 850, 750);
 
-    glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(0, width, 0, height, -1, 1);
     glMatrixMode(GL_MODELVIEW);
+
+    bool **field = CreateField(gridSize, gridSize);
+    bool **newField = CreateField(gridSize, gridSize);
+    field[4][4] = 1;
+    field[4][5] = 1;
+    field[3][6] = 1;
+    field[4][5] = 1;
+    field[2][5] = 1;
+    field[3][5] = 1;
+    field[7][7] = 1;
+    field[16][20] = 1;
+    field[16][21] = 1;
+    field[15][20] = 1;
+    field[14][19] = 1;
+    field[15][19] = 1;
+    field[14][14] = 1;
+    field[14][15] = 1;
+    field[14][17] = 1;
 
     while (!glfwWindowShouldClose(window))
     {
@@ -99,13 +165,30 @@ int main(void){
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
         glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT); 
+        glClear(GL_COLOR_BUFFER_BIT);
         
         drawGrid(gridSize);
 
+        double currentTime = glfwGetTime();
+
+        glfwSetKeyCallback(window, key_space);
+        
+        fillField(field, gridSize);
+        
+        if (!shouldWait && currentTime - lastStepTime >= stepDelay) {
+            NextStep(field, newField, gridSize, gridSize);
+            swapFields(&field, &newField, gridSize);
+            lastStepTime = currentTime;
+        }
+
+        fillField(newField, gridSize);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    FreeField(field, gridSize);
+    FreeField(newField, gridSize);
 
     glfwTerminate();
     return 0;
